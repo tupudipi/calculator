0502291661d349f27295e0799bb467d99ebf7b36
tupudipi <tupudipi@gmail.com>
2021-11-03 13:05:08 +0200

    changed body bg to whitesmoke, added rounding to numbers with many decimals

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
-function add(a, b){
-    return a+b;
-}
-
-function subtract(a, b){
-    return a-b;
-}
-
-function multiply(a, b){
-    return a*b;
-}
-
-function divide(a, b){
-    return a/b;
-}
-
-function power(a, b){
-    return a**b;
-}
-
 function operate(operator, a, b){
     switch (operator){
         case '+':
-            return add(a,b);
+            return (a+b).toFixed(2);
             break;
         case '-':
-            return subtract(a, b);
+            return (a-b).toFixed(2);
             break;
         case '*':
-            return multiply(a, b);
+            return (a*b).toFixed(2);
             break;
         case '/':
-            return divide(a, b);
+            return (a/b).toFixed(2);
             break;
         case '^':
-            return power(a, b);
+            return (a**b).toFixed(2);
             break;
     }
 }
