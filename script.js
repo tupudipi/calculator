a8072c84a14f1eeb721b0d2636b3039ddbe2e8e9
tupudipi <tupudipi@gmail.com>
2021-11-03 13:00:06 +0200

    gave up and used the FCC projec

--- script.js
+++ script.js
@@ -38,85 +38,118 @@ function operate(operator, a, b){
     }
 }
 
-let currentOpperationDisplay = document.getElementById("curr");
-let prevOpperationDisplay = document.getElementById("prev");
+let displayCurrent = document.getElementById("curr");
+let calculator = document.getElementById("calculator");
+let displayPrevious = document.getElementById("prev");
 let allOpperations = ['/', '*', '-', '+', '%', '^'];
-let selectedOpp = ''; 
-let firstOpperand, secondOpperand;
 
 window.addEventListener('keydown', checkKey);
 let keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('click', check));
+keys.forEach(key => key.addEventListener('click', checkClick));
 
-let opperating = false;
-
-function check(e){
+function checkClick(e) {
     let pressedKey = e.target.id;
-    console.log (pressedKey);
-
-    if(opperating){
-        if(pressedKey == 'ac')
-        {
-            currentOpperationDisplay.innerText = '';
-            prevOpperationDisplay.innerText = '';
-            selectedOpp = '';
-            
-        }
-        else if(pressedKey == 'c')
-        {
-            let str = currentOpperationDisplay.innerText;
-            str = str.substring(0, str.length-1);
-            currentOpperationDisplay.innerText = str;
-        }
-        else if(pressedKey == '=')
-        {
-            prevOpperationDisplay.innerText += pressedKey;
-            console.log(prevOpperationDisplay.innerText);
-            currentOpperationDisplay.innerText = operate(selectedOpp, +firstOpperand, +secondOpperand);
-        }
-        else {
-            currentOpperationDisplay.innerText += pressedKey;
-            prevOpperationDisplay.innerText += pressedKey;
-            secondOpperand=pressedKey;
-            console.log(firstOpperand, selectedOpp, secondOpperand);
-        }
-    }
-
-    if(!opperating)
-    {
-        if (allOpperations.includes(pressedKey)){
-            firstOpperand = currentOpperationDisplay.innerText;
-            selectedOpp = pressedKey;
-            prevOpperationDisplay.innerText = (firstOpperand + selectedOpp);
-            currentOpperationDisplay.innerText = "";
-            opperating = true;
-        } 
-        else if(pressedKey == 'ac')
-        {
-            currentOpperationDisplay.innerText = '';
-            prevOpperationDisplay.innerText = '';
-            firstOpperand = null;
-            secondOpperand = null;
-            selectedOpp = '';
-        }
-        else if(pressedKey == 'c')
-        {
-            let str = currentOpperationDisplay.innerText;
-            str = str.substring(0, str.length-1);
-            currentOpperationDisplay.innerText = str;
-        }
-        else {
-            currentOpperationDisplay.innerText += pressedKey;
-        }
-    }
-}
-
-function readSecondOpperand(e){
-    return e.target.id;
+    calculate(pressedKey);
 }
 
 function checkKey(e){
     let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    let pressedKey = e.target.id;
+    let pressedKey = key.id;
+    calculate(pressedKey);
 }
+
+function calculate(x){
+    let pressedKey = x;
+    let displayedNum = displayCurrent.textContent;
+    let previousKeyType = calculator.dataset.previousKeyType;
+
+    if(allOpperations.includes(pressedKey)) {
+        //console.log('operation: ' + pressedKey);
+        
+        const firstValue = calculator.dataset.firstOpperand;
+        const operator = calculator.dataset.selectedOpp;
+        const secondValue = displayedNum;
+
+        if (   firstValue 
+            && operator 
+            && previousKeyType !== 'operator' 
+            && previousKeyType !== 'resultPlease') 
+        {
+            const calcValue = operate(operator, +firstValue, +secondValue);
+            displayCurrent.textContent = calcValue;
+            calculator.dataset.firstOpperand = calcValue;
+        } 
+        else {
+            calculator.dataset.firstOpperand = displayedNum;
+        }
+
+        displayPrevious.textContent = displayCurrent.textContent + pressedKey;
+        calculator.dataset.selectedOpp = pressedKey;
+        calculator.dataset.previousKeyType = 'operator';
+    }
+    else {
+        switch(pressedKey){
+            case '.':
+                //console.log('decimal');
+                if(!displayedNum.includes('.')){
+                    displayCurrent.textContent = displayedNum + '.';
+                }
+                if (previousKeyType === 'operator' || previousKeyType === 'resultPlease') {
+                    displayCurrent.textContent = '0.';
+                }
+                calculator.dataset.previousKeyType = 'decimal';
+                break;
+
+            case 'ac':
+                //console.log('clear all');
+                calculator.dataset.firstOpperand = '';
+                calculator.dataset.modValue = '';
+                calculator.dataset.operator = '';
+                displayCurrent.textContent = '0';
+                displayPrevious.textContent = '';
+                calculator.dataset.previousKeyType = 'clearAll';
+                break;
+
+            case 'c':
+                //console.log('delete one');
+                displayCurrent.textContent = displayCurrent.textContent.slice(0, -1);
+                calculator.dataset.previousKeyType = 'deleteOne';
+                break;
+
+            case '=':
+                //console.log('result please');
+                let firstOpperand = calculator.dataset.firstOpperand;
+                let selectedOpp = calculator.dataset.selectedOpp;
+                let secondOpperand = displayedNum;
+
+                if(firstOpperand){
+                    if(previousKeyType === 'resultPlease'){
+                        firstOpperand = displayedNum;
+                        secondOpperand = calculator.dataset.modValue;
+                        displayPrevious.textContent = displayedNum + selectedOpp + secondOpperand + '=';
+                    } else {
+                        displayPrevious.textContent += secondOpperand + '=';
+                    }
+                    displayCurrent.textContent = operate(selectedOpp, +firstOpperand, +secondOpperand);
+                }
+
+                calculator.dataset.modValue = secondOpperand;
+                calculator.dataset.previousKeyType = 'resultPlease'
+                break;
+
+            default:
+                //console.log('number key: ' + pressedKey);
+                if(displayedNum === '0' 
+                || previousKeyType === 'operator' 
+                || previousKeyType === 'resultPlease'){
+                    displayCurrent.textContent = pressedKey;
+                } else {
+                    displayCurrent.textContent = displayedNum + pressedKey;
+                }
+                calculator.dataset.previousKeyType = 'number;'
+                break;
+        }
+    }
+}
+    
 
